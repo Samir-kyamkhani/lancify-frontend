772dcc51c2a2a694d7b2b4b980bba9029a02ca70
Itsarbaz51 <itsarbaz51@gmail.com>
2025-05-15 19:00:08 +0530

    signup and login done

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import {
@@ -15,23 +14,16 @@ import ProposalsPage from "./pages/dashboard/ProposalsPage";
 import ProjectPage from "./pages/dashboard/ProjectPage";
 import InvoicePage from "./pages/dashboard/InvoicePage";
 import TeamPage from "./pages/dashboard/TeamPage";
-// import ReportsPage from "./pages/dashboard/ReportsPage";
 import IntegrationsPage from "./pages/dashboard/IntegrationsPage";
 import InboxPage from "./pages/dashboard/InboxPage";
-// import SocialMediaManagementPage from "./pages/dashboard/SocialMediaManagementPage";
 import ChatPage from "./pages/dashboard/ChatPage";
 import SupportPage from "./pages/dashboard/SupportPage";
 import ProfilePage from "./pages/dashboard/ProfilePage";
 import ClientDetailedPage from "./pages/dashboard/detailedPage/ClientDetailedPage";
 import InvoicesDetailedPage from "./pages/dashboard/detailedPage/InvoicesDetailedPage";
 import ProposalDetailsPage from "./pages/dashboard/detailedPage/ProposalDetailsPage";
-import UserProjectPage from "./pages/clientDashboard/UserProjectPage";
-import UserDashboardPage from "./pages/clientDashboard/UserDashboardPage";
+
 import ProjectDetailedPage from "./pages/clientDashboard/detailedPage/ProjectDetailedPage";
-import UserProposalsPage from "./pages/clientDashboard/UserProposalsPage";
-import UserProposalDetailsPage from "./pages/clientDashboard/detailedPage/UserProposalDetailsPage";
-import UserInvoicesPage from "./pages/clientDashboard/UserInvoicesPage";
-import UserInvoiceDetailsPage from "./pages/clientDashboard/detailedPage/UserInvoicesDetailedPage";
 import UserSharedFilesPage from "./pages/clientDashboard/UserSharedFilesPage";
 
 import Layout from "./components/landingComponents/Layout/Layout";
@@ -40,82 +32,78 @@ import BlogsPage from "./pages/landingPages/BlogsPage";
 import ContactPage from "./pages/landingPages/ContactPage";
 import SignupPage from "./pages/landingPages/SignupPage";
 import LoginPage from "./pages/landingPages/LoginPage";
+
 import { Provider } from "react-redux";
 import { store } from "./app/store";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+// import ProtectedRoute from "./components/ProtectedRoute";
+// import LogoutPage from "./components/LogoutPage";
+// import NotFoundPage from "./components/NotFoundPage";
+
+// Optional: Not Found Page (you can create this component if needed)
+// import NotFoundPage from "./pages/NotFoundPage";
 
 const client_id = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
-      <Route path="/dashboard" element={<DashboardLayout />}>
-        <Route index element={<Dashboard />} />
-        <Route path="clients" element={<ClientsPage />} />
-        <Route path="proposals" element={<ProposalsPage />} />
-        <Route path="projects" element={<ProjectPage />} />
-        <Route path="payment" element={<InvoicePage />} />
-        <Route path="teams" element={<TeamPage />} />
-        {/* <Route path="reports" element={<ReportsPage />} /> */}
-        <Route path="integrations" element={<IntegrationsPage />} />
-        <Route path="inbox" element={<InboxPage />} />
-        {/* <Route path="social-manager" element={<SocialMediaManagementPage />} /> */}
-        <Route path="chat" element={<ChatPage />} />
-        <Route path="support" element={<SupportPage />} />
-        <Route path="profile" element={<ProfilePage />} />
-
-        {/* Detailed pages */}
-        <Route path="/dashboard/client/:id" element={<ClientDetailedPage />} />
+      {/* Public Routes */}
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="blogs" element={<BlogsPage />} />
+        <Route path="contact" element={<ContactPage />} />
         <Route
-          path="/dashboard/payment/:id"
-          element={<InvoicesDetailedPage />}
-        />
-        <Route
-          path="/dashboard/proposal/:id"
-          element={<ProposalDetailsPage />}
-        />
-      </Route>
-      <Route path="/user" element={<DashboardLayout />}>
-        <Route index element={<UserDashboardPage />} />
-        <Route path="/user/projects" element={<UserProjectPage />} />
-        <Route path="/user/project/:id" element={<ProjectDetailedPage />} />
-        <Route path="/user/proposals" element={<UserProposalsPage />} />
-        <Route
-          path="/user/proposal/:id"
-          element={<UserProposalDetailsPage />}
-        />
-        <Route path="/user/payments" element={<UserInvoicesPage />} />
-        <Route path="/user/payment/:id" element={<UserInvoiceDetailsPage />} />
-        <Route path="/user/files" element={<UserSharedFilesPage />} />
-      </Route>
-      <Route
-        path="/"
-        element={
-          <Provider store={store}>
-            <Layout />
-          </Provider>
-        }
-      >
-        <Route index={true} element={<HomePage />} />
-        <Route path="/blogs" element={<BlogsPage />} />
-        <Route path="/contact" element={<ContactPage />} />
-        <Route
-          path="/signup"
+          path="signup"
           element={
             <GoogleOAuthProvider clientId={client_id}>
               <SignupPage />
             </GoogleOAuthProvider>
           }
         />
-        <Route path="/login" element={<LoginPage />} />
+        <Route
+          path="login"
+          element={
+            <GoogleOAuthProvider clientId={client_id}>
+              <LoginPage />
+            </GoogleOAuthProvider>
+          }
+        />
       </Route>
-      ,
+
+      {/* Protected Dashboard Routes */}
+      {/* <Route element={<ProtectedRoute />}> */}
+      <Route path="/dashboard" element={<DashboardLayout />}>
+        <Route index element={<Dashboard />} />
+        <Route path="clients" element={<ClientsPage />} />
+        <Route path="proposals" element={<ProposalsPage />} />
+        <Route path="projects" element={<ProjectPage />} />
+        <Route path="project/:id" element={<ProjectDetailedPage />} />
+        <Route path="payments" element={<InvoicePage />} />
+        <Route path="teams" element={<TeamPage />} />
+        <Route path="integrations" element={<IntegrationsPage />} />
+        <Route path="inbox" element={<InboxPage />} />
+        <Route path="chat" element={<ChatPage />} />
+        <Route path="support" element={<SupportPage />} />
+        <Route path="profile" element={<ProfilePage />} />
+        <Route path="client/:id" element={<ClientDetailedPage />} />
+        <Route path="payment/:id" element={<InvoicesDetailedPage />} />
+        <Route path="files" element={<UserSharedFilesPage />} />
+        <Route path="proposal/:id" element={<ProposalDetailsPage />} />
+        {/* <Route path="logout" element={<LogoutPage />} /> */}
+      </Route>
+      {/* </Route> */}
+
+      {/* Optional Not Found Route */}
+      {/* <Route path="*" element={<NotFoundPage />} /> */}
     </>
   )
 );
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
+const root = createRoot(document.getElementById("root"));
+
+root.render(
+  <Provider store={store}>
     <RouterProvider router={router} />
-  </StrictMode>
+  </Provider>
 );
